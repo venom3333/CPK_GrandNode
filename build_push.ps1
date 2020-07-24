b72a134d6fa0bbaf0c5ea8489a22de56ce5e21d1
Vladimir <venom3333@gmail.com>
2020-07-24 14:29:51 +0300

    nano updates

--- build_push.ps1
+++ build_push.ps1
@@ -1,4 +1,5 @@
 $REGISTRY_ID = (yc container registry get --name alrosa-test-container-registry  --format json | ConvertFrom-Json).id
 docker build -t cr.yandex/${REGISTRY_ID}/cpk:development .
 docker push cr.yandex/${REGISTRY_ID}/cpk:development
+kubectl rollout restart deployment/cpk-dev-deployment
 CMD /c PAUSE
